938d33afa9717a9b0c0861fdf8029c36c8cff131
Charles <charlespogzz@googlemail.com>
2021-01-29 23:54:52 +0800

    added some to conditions

--- compare.sh
+++ compare.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-
+export ds="/var/log"
+file1="$ds/backup_current_users"
+file2="$ds/current_users"
 
 hash_it() {
-        cut -d : -f 1,6 /etc/passwd | tee -a results.txt | md5sum | cut -d' ' -f1 > backup_current_users
+        cut -d : -f 1,6 /etc/passwd | tee -a $ds/results.txt | md5sum | cut -d' ' -f1 > $ds/backup_current_users
 }
 
 appendcontent() {
-        cat backup_current_users | tee -a current_users | echo "`date`" >> current_users
+        cat $ds/backup_current_users | tee -a $ds/current_users | echo "`date`" >> $ds/current_users
 }
 
 
 hash_it
 
-
-file1="/home/sab/Desktop/gitlab_exam/backup_current_users"
-file2="/home/sab/Desktop/gitlab_exam/current_users"
-
-if cmp -s "$file1" "$file2"; then
+if [[ ! -e $ds/current_users ]]; then
+	touch $ds/results.txt $ds/backup_current_users $ds/current_users
+elif cmp -s "$file1" "$file2"; then
         printf "same files, no changes\n\n"
 else
         appendcontent
-
 fi
 
